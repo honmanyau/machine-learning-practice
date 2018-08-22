97153c2fdf78d4ecf35b224938c3a90469ef9611
honmanyau <honmanyau@users.noreply.github.com>
2018-08-22 13:00:48 +1000

    Added error handling for existing directory.

--- javascript/knn-fisher-iris-dataset/data/cleanRaw.ts
+++ javascript/knn-fisher-iris-dataset/data/cleanRaw.ts
@@ -3,15 +3,12 @@ import * as fs from 'fs';
 // ===========
 // == Setup ==
 // ===========
-const targetDirName = '/raw-cleaned';
-const sourceDirPath = __dirname + '/raw';
-const targetDirPath = __dirname + targetDirName;
+const TARGET_DIR = 'raw-cleaned';
+const SOURCE_PATH = __dirname + '/raw';
+const TARGET_PATH = __dirname + '/' + TARGET_DIR;
 
 const readOptions = { encoding: 'utf-8' };
 const writeOptions = { encoding: 'utf-8' };
-
-const listOfFiles = fs.readdirSync(__dirname);
-const targetDirectoryExists = !!~listOfFiles.indexOf(targetDirName);
 
 // ==========
 // == Main ==
@@ -20,21 +17,19 @@ let setosaData: string;
 let virginicaData: string;
 let versicolorData: string;
 
-setosaData = fs.readFileSync(sourceDirPath + '/setosa.txt', readOptions);
-virginicaData = fs.readFileSync(sourceDirPath + '/virginica.txt', readOptions);
-versicolorData = fs.readFileSync(sourceDirPath + '/versicolor.txt', readOptions);
+prepareTargetDirectory();
+
+setosaData = fs.readFileSync(SOURCE_PATH + '/setosa.txt', readOptions);
+virginicaData = fs.readFileSync(SOURCE_PATH + '/virginica.txt', readOptions);
+versicolorData = fs.readFileSync(SOURCE_PATH + '/versicolor.txt', readOptions);
 
 setosaData = cleanDecimalPoints(setosaData, 'setosa');
 virginicaData = cleanDecimalPoints(virginicaData, 'virginica');
 versicolorData = cleanDecimalPoints(versicolorData, 'versicolor');
 
-if (!targetDirectoryExists) {
-  fs.mkdirSync(targetDirPath);
-}
-
-fs.writeFileSync(targetDirPath + '/setosa', setosaData, writeOptions);
-fs.writeFileSync(targetDirPath + '/virginica', virginicaData, writeOptions);
-fs.writeFileSync(targetDirPath + '/versicolor', versicolorData, writeOptions);
+fs.writeFileSync(TARGET_PATH + '/setosa', setosaData, writeOptions);
+fs.writeFileSync(TARGET_PATH + '/virginica', virginicaData, writeOptions);
+fs.writeFileSync(TARGET_PATH + '/versicolor', versicolorData, writeOptions);
 
 // ===============
 // == Functions ==
@@ -60,4 +55,30 @@ function cleanDecimalPoints(data: string, species: Species): string {
   });
 
   return lines.join('\n');
+}
+
+/**
+ * This function creates a directory for storing cleaned data if it does not
+ * already exist.
+ * @returns {boolean} {@code true} if the target directory does not exist,
+ *     {@code false} otherwise.
+ */
+function prepareTargetDirectory(): boolean {
+  const listOfFiles = fs.readdirSync(__dirname);
+  const targetDirectoryExists = !!~listOfFiles.indexOf(TARGET_DIR);
+
+  if (!targetDirectoryExists) {
+    fs.mkdirSync(TARGET_PATH);
+  }
+  else {
+    console.error([
+      '',
+      'Program terminated! Target directory already exists:',
+      TARGET_PATH,
+      ''
+    ].join('\n'));
+    process.exit();
+  }
+
+  return !targetDirectoryExists;
 }
