83181dea5c38d516be459cc4c72e8003b2577423
honmanyau <honmanyau@users.noreply.github.com>
2018-09-04 16:23:09 +1000

    Added crude ml-knn TypeScript definitions.

---
+++ javascript/knn-fisher-iris-dataset/analysis/src/ml-knn.d.ts
@@ -0,0 +1,14 @@
+// declare class KNN {
+//     constructor(
+//       dataset: number[],
+//       labels: number[],
+//       options?: {
+//         k: number,
+//         distance: (u: number[], v: number[]) => number
+//       }
+//     );
+//
+//     public predict(dataset: number[]): number[];
+// }
+//
+// export = KNN;
