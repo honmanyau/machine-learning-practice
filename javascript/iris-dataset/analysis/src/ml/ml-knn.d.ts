e4f5170e5e0d2028d0926595d23f254be604ab71
honmanyau <honmanyau@users.noreply.github.com>
2018-09-05 23:37:31 +1000

    Moved type definition, commented code out and added instructions.

---
+++ javascript/iris-dataset/analysis/src/ml/ml-knn.d.ts
@@ -0,0 +1,16 @@
+// This type definition should be added to node_modules/@types and uncommented
+
+// declare module 'ml-knn' {
+//   export default class KNN {
+//       constructor(
+//         dataset: number[][],
+//         labels: any[],
+//         options?: {
+//           k?: number,
+//           distance?: (u: number[], v: number[]) => number
+//         }
+//       );
+//
+//       public predict(dataset: any[]): number[];
+//   }
+// }
